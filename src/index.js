c8b8f4326514b5faf6fa2924260ab9528b958f2e
Andre Torbitoni <andre.torbitoni@gmail.com>
2020-01-27 22:37:30 -0500

    App file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import FormContainer from './js/components/container/FormContainer.jsx'
+import App from './js/App.jsx'
 
 const wrapper = document.getElementById('app')
-const jsx = <FormContainer />
+const jsx = <App />
 
 ReactDOM.render(jsx, wrapper)
